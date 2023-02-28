b33a4a6d3458f8fde9456be3e42f26dcb7acdf55
Lerato Bale <lerato.l.bale@gmail.com>
2023-02-28 12:09:44 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * Return: Numbers from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
