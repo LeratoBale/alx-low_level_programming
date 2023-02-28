834151de58c319e36f43a7e669921513b968dc84
Lerato Bale <lerato.l.bale@gmail.com>
2023-02-28 06:30:09 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: The number to be checked
+ * Return: 1 if c is a digit, 0 if otherwise.
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
