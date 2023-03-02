5e50c41a2247e5ca5039b432898d2f9073ca61f7
Lerato Bale <lerato.l.bale@gmail.com>
2023-03-02 14:07:56 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,15 @@
 /**
  * _puts - prints a string
  * @str: string to print
+ * Return: void
  */
 
 void _puts(char *str)
 
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-	_putchar(*str++);
+	_putchar(*str);
 	}
 	_putchar('\n');
 }
